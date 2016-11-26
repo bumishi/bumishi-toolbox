8efd328661232da24906c18af37bbbb7d79543e1
不迷失 <1060420897@qq.com>
2016-11-26 22:50:21 +0800

    add restresponse model

---
+++ src/main/java/org/bumishi/toolbox/model/RestResponse.java
@@ -0,0 +1,70 @@
+package org.bumishi.toolbox.model;
+
+/**
+ * Restful api response model
+ * Created by xieqiang on 2016/11/26.
+ */
+public class RestResponse {
+
+    private boolean success;
+
+    private String code;
+
+    private String msg;
+
+    private Object data;
+
+    public boolean success() {
+        return success;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+
+    public static RestResponse ok(){
+        RestResponse rep=new RestResponse();
+        rep.success=true;
+        return rep;
+    }
+
+
+    public static RestResponse ok(Object data){
+        RestResponse rep=new RestResponse();
+        rep.success=true;
+        rep.data=data;
+        return rep;
+    }
+
+    public static RestResponse fail(){
+        RestResponse rep=new RestResponse();
+        rep.success=false;
+        return rep;
+    }
+
+    public static RestResponse fail(String msg){
+        RestResponse rep=new RestResponse();
+        rep.success=false;
+        rep.msg=msg;
+        return rep;
+    }
+
+    public static RestResponse fail(String code,String msg){
+        RestResponse rep=new RestResponse();
+        rep.success=false;
+        rep.msg=msg;
+        rep.code=code;
+        return rep;
+    }
+
+
+}
